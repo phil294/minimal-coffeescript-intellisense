eb03907af4aae375611e93109294d860d250bf43
phil294 <philip@waritschlager.de>
2021-07-30 02:24:00 +0200

    Provide CS and TS diagnostics

--- README.md
+++ README.md
@@ -2,13 +2,31 @@
 
 ## Functionality
 
-This extension provides Intellisense for CoffeeScript. This works not by leveraging CS AST but by compiling it to JS and asking tsserver for completions, diagnostics etc., while mapping the results using source maps.
+This extension provides Intellisense, syntax checking and type checking for CoffeeScript. This works not by leveraging CS AST but by compiling it to JS and asking tsserver for completions, diagnostics etc., while mapping the results using source maps.
+
+### Features
+
+- Syntax checking: CS -> JS compilation errors
+- Type checking using TS based on JS, function signatures, JSDoc, TS dependencies. Check out [JS Projects Utilizing TypeScript]https://www.typescriptlang.org/docs/handbook/intro-to-js-ts.html) on how to leverage this the best way. A `//@ts-check` is inserted automatically for you at the beginning of the file.
+	((gif:
+		x = a: 1
+		x.a = 'text' # error
+	))
+- The following IntelliSense features of the Language Server Protocol are implemented:
+	- TODO
+
+```
+unrelated TODOs:
+vscode suggestions should show extension name?
+```
 
 Although this works great in principle, there are limitations to this approach:
 - No complex source code altering features can be implemented like refactoring, formatting, snippets
 - Local context can sometimes be missing in autocompletion (TODO explain this)
+- Performance is probably pretty horrible / resource intensive. Should not be *too* bad though, as this extension builds on the shoulders of blazingly fast compilers and just glues them together
 
-Overall, the following features of the Language Server Protocol are implemented:
-TODO
+Not yet implemented but possible in principle:
+- Cross-file autocompletion: Import autocompletion works if the imported module is native JavaScript. If you are trying to import from another CS file, that file won't be compiled and parsed yet
+- Local context autocompletion: Autocomplete is currently only based on global (editor-wide) declarations, so you cannot get autocomplete for object members, class members, function parameters etc. This has high priority but is not done yet
 
 TODO rewrite this extension using coffeescript, proving its potential usefulness
--- extension.js
+++ extension.js
@@ -1,37 +1,106 @@
 //@ts-check
-const { commands, ExtensionContext, languages, Position, Range, Uri, workspace, CompletionList, Diagnostic, DiagnosticSeverity } = require('vscode')
+const { commands, ExtensionContext, languages, Position, Range, Uri, workspace, CompletionList, Diagnostic, DiagnosticSeverity, window } = require('vscode')
 const CoffeeScript = require('coffeescript')
+//@ts-ignore
+const VolatileMap = require('volatile-map').default
+//@ts-ignore
+const MD5 = new (require('jshashes').MD5)()
+const ts_morph = require('ts-morph')
 
 const language = 'coffeescript'
+const ext_identifier = 'coffeescript-intellisense-js-based'
+
+/** @type {NodeJS.Timeout} */
+let coffee_compile_debouncer
+/** @type {NodeJS.Timeout} */
+let ts_compile_debouncer
+
+/** @type {Map<string,ReturnType<coffee_compile>>} */
+const compilation_cache = new VolatileMap(60000)
+
+/** @type {ts_morph.Project} */
+let ts_project
+/** @type {ts_morph.SourceFile} */
+let ts_source_file
 
 /**
  * Attempts to compile and return js and sourceMap, or, if failing to do so,
- * compile error diagnostics
+ * compile error diagnostics. Caches results for several seconds.
  * @param text {string}
- * returns {{diagnostics?: Diagnostic[], js?: string, source_map?: Array<{line:number,columns:Array<{line:number,column:number,sourceLine:number,sourceColumn:number}|undefined>}|undefined>}} TODO wrong in repo bc columns is not array?
+ * returns {{ diagnostics?: Diagnostic[], js?: string, source_map?: CoffeeScript.LineMap[] }}
+ * @returns {{diagnostics?: Diagnostic[], js?: string, source_map?: Array<{line:number,columns:Array<{line:number,column:number,sourceLine:number,sourceColumn:number}|undefined>}|undefined>}} TODO wrong in repo bc columns is not array?
  */
-function compile(text) {
+function coffee_compile(text) {
+	const hash = MD5.hex(text)
+	const cached = compilation_cache.get(hash)
+	if (cached)
+		return cached
+	let result
 	try {
-		const result = CoffeeScript.compile(text, { sourceMap: true, bare: true })
-		return {
-			source_map: result.sourceMap.lines,
-			js: result.js.trim()
+		/** @type {any} TODO **/
+		const response = CoffeeScript.compile(text, { sourceMap: true, bare: true })
+		result = {
+			source_map: response.sourceMap.lines,
+			js: response.js.trim()
 		}
-	} catch(e) {
-		if(e.name !== "SyntaxError")
+	} catch (e) {
+		if (e.name !== "SyntaxError")
 			throw e
 		const l = e.location
-		return {
-			diagnostics: [ new Diagnostic(new Range(l.first_line, l.first_column, l.last_line, l.last_column + 1), e.message, DiagnosticSeverity.Error) ]
+		result = {
+			diagnostics: [new Diagnostic(new Range(l.first_line, l.first_column, l.last_line, l.last_column + 1), e.message, DiagnosticSeverity.Error)]
 		}
 	}
+	compilation_cache.set(hash, result)
+	return result
+}
+/**
+ * Compiles ts code into js and returns only diagnostics
+ * @param text {string}
+ * @returns {Promise<ReturnType<coffee_compile>>}
+ */
+async function ts_compile(text) {
+	const hash = MD5.hex(text)
+	const cached = compilation_cache.get(hash)
+	if (cached)
+		return cached
+	// For JS type checking (using tsserver), there does not seem to be any vscode api available
+	// as diagnostics are always push-only. So we need to spin up yet another server:
+	if (!ts_project) {
+		ts_project = new ts_morph.Project({
+			useInMemoryFileSystem: true,
+			compilerOptions: {
+				allowJs: true,
+				alwaysStrict: true,
+				noImplicitAny: true,
+				strictNullChecks: true,
+			}
+		})
+		// Can't easily use `.js` in this library, but `.ts` does the same thing anyway
+		ts_source_file = ts_project.createSourceFile(ext_identifier + ".ts", '')
+	}
+	ts_source_file.replaceWithText(text)
+	await ts_project.save()
+	// Very slow: https://github.com/dsherret/ts-morph/issues/1182
+	const pre_emit_diagnostics = ts_project.getPreEmitDiagnostics()
+	const result = { diagnostics: pre_emit_diagnostics.map(d => {
+		const m = d.getMessageText()
+		const message = typeof m === 'string' ? m : m.getMessageText()
+		return new Diagnostic(new Range(
+			// ts-morph only returns line and total pos, not inline character.
+			// Transforming this is annoying so we just highlight the whole line (TODO)
+			d.getLineNumber() || 0, 0, d.getLineNumber() || 0, 0
+		), message, DiagnosticSeverity.Error)
+	}) }
+	compilation_cache.set(hash, result)
+	return result
 }
 
 function activate(/** @type {ExtensionContext} */ { subscriptions }) {
 
-	const diagnostic_collection = languages.createDiagnosticCollection('ext-compiling-coffee') // todo replace with final extension name
+	const diagnostic_collection = languages.createDiagnosticCollection(ext_identifier)
 	subscriptions.push(diagnostic_collection)
-  
+
 	const virtual_document_contents = new Map()
 
 	/* See README for how this extension works.
@@ -39,16 +108,52 @@ function activate(/** @type {ExtensionContext} */ { subscriptions }) {
 	are necessary. This approach is based on "Embedded Languages / Request Forwarding" @
 	https://code.visualstudio.com/api/language-extensions/embedded-languages#request-forwarding-sample.
 	*/
-	subscriptions.push(workspace.registerTextDocumentContentProvider('ext-compiled-javascript', { // todo replace with final extension name
+	subscriptions.push(workspace.registerTextDocumentContentProvider(ext_identifier, {
+		// This is a lookup service that is unfortunately required and otherwise not really interesting
 		provideTextDocumentContent: uri => {
+			// Remove leading slash and trailing `.js`
 			const original_uri = uri.path.slice(1).slice(0, -3)
 			const decoded_uri = decodeURIComponent(original_uri)
 			const virtual_content = virtual_document_contents.get(decoded_uri)
 			return virtual_content
 		}
 	}))
+	subscriptions.push(workspace.onDidChangeTextDocument(change_event => {
+		const active_document = change_event.document
+		if (active_document !== window.activeTextEditor?.document)
+			return
+		diagnostic_collection.clear()
+		if (coffee_compile_debouncer)
+			clearTimeout(coffee_compile_debouncer)
+		coffee_compile_debouncer = setTimeout(async () => {
+			const { js, source_map: coffee_source_map, diagnostics: coffee_diagnostics } = coffee_compile(active_document.getText())
+			if (!js) {
+				diagnostic_collection.set(active_document.uri, coffee_diagnostics)
+				return
+			}
+
+			if (ts_compile_debouncer)
+				clearTimeout(ts_compile_debouncer)
+			ts_compile_debouncer = setTimeout(async () => {
+				const { diagnostics: ts_diagnostics = [] } = await ts_compile(js)
+				for(const d of ts_diagnostics) {
+					// const mapped_start = coffee_source_map?.[d.range.start.line - 1]?.columns[d.range.start.character]
+					// const mapped_end = coffee_source_map?.[d.range.end.line - 1]?.columns[d.range.end.character]
+					// d.range = new Range(
+					// 	new Position(mapped_start?.sourceLine || 0, mapped_start?.sourceColumn || 0),
+					// 	new Position(mapped_end?.sourceLine || 0, mapped_end?.sourceColumn || 0))
+					const mapped_start = coffee_source_map?.[d.range.start.line - 1]?.columns.find(Boolean)
+					const mapped_end = coffee_source_map?.[d.range.end.line - 1]?.columns.find(Boolean)
+					d.range = new Range(
+						new Position(mapped_start?.sourceLine || 0, 0),
+						new Position(mapped_end?.sourceLine || 0, Number.MAX_VALUE))
+				}
+				diagnostic_collection.set(active_document.uri, ts_diagnostics)
+			}, 1)
+		}, 500)
+	}))
 	subscriptions.push(
-        languages.registerCompletionItemProvider(language, {
+		languages.registerCompletionItemProvider(language, {
 			async provideCompletionItems(document, real_coffee_position, token) {
 				let coffee = document.getText()
 				const current_line = document.lineAt(real_coffee_position.line).text
@@ -56,8 +161,8 @@ function activate(/** @type {ExtensionContext} */ { subscriptions }) {
 
 				// Proper lsp servers can handle autocompletion requests even when the surrounding code is invalid.
 				// This is not possible with this extension so we first try to compile as is...
-				let compile_result = compile(coffee)
-				if(!compile_result.js) {
+				let compile_result = coffee_compile(coffee)
+				if (!compile_result.js) {
 					// ... and when that failed, replace the current line (indented) with `true` and try again.
 					coffee = [
 						coffee.substr(0, document.offsetAt(real_coffee_position.with(undefined, 0))),
@@ -65,9 +170,9 @@ function activate(/** @type {ExtensionContext} */ { subscriptions }) {
 						'true',
 						coffee.substr(document.offsetAt(real_coffee_position.with(undefined, current_line.length)))
 					].join('')
-					compile_result = compile(coffee)
+					compile_result = coffee_compile(coffee)
 				}
-				if(!compile_result.js || !compile_result.source_map)
+				if (!compile_result.js || !compile_result.source_map)
 					return
 
 				// Completion in the JS however *can* be based on invalid code, so we insert the current line again.
@@ -78,21 +183,22 @@ function activate(/** @type {ExtensionContext} */ { subscriptions }) {
 
 				const original_uri = document.uri.toString()
 				virtual_document_contents.set(original_uri, js)
-				const virtual_document_uri = Uri.parse(`ext-compiled-javascript://compiled/${encodeURIComponent(original_uri)}.js`)
+				// The `.js` is what makes vscode turn to tsserver for the completion request
+				const virtual_document_uri = Uri.parse(`${ext_identifier}://compiled/${encodeURIComponent(original_uri)}.js`)
 				// , context.triggerCharacter v todo
 				/** @type {CompletionList|undefined} */
 				const completion_list = await commands.executeCommand('vscode.executeCompletionItemProvider', virtual_document_uri, emulated_cursor_position)
-				
+
 				// Now the completions are there based on JS but their position does not fit to coffee. Complicated backwards
 				// mapping is not necessary as they can all simply be mapped to current coffee cursor position:
 				const real_coffee_position_one_left = real_coffee_position.with(undefined, real_coffee_position.character - 1)
-				for(const item of completion_list?.items||[]) {
-					if(item.range instanceof Range) {
+				for (const item of completion_list?.items || []) {
+					if (item.range instanceof Range) {
 						const range = new Range(real_coffee_position_one_left, real_coffee_position)
 						item.range = range
-						if(item.textEdit)
+						if (item.textEdit)
 							item.textEdit.range = range
-					} else if(item.range?.replacing) {
+					} else if (item.range?.replacing) {
 						const range = new Range(real_coffee_position, real_coffee_position)
 						item.range.replacing = range
 						item.range.inserting = range
@@ -101,11 +207,5 @@ function activate(/** @type {ExtensionContext} */ { subscriptions }) {
 				return completion_list?.items
 			}
 		}, '.', '\"'))
-		// let compile_result = compile(coffee)
-		// if(!compileResult.js || !compileResult.source_map) {
-		// 	diagnostic_collection.clear()
-		// 	diagnostic_collection.set(document.uri, compileResult.diagnostics)
-		// 	return
-		// }
 }
 module.exports = { activate }
